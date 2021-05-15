83e6665b40dcee0a7b346294b7e397c1f79cc3ba
enrip99 <enric.1999@hotmail.com>
2021-05-15 15:45:22 +0200

    tweaked help command, added horario file

--- commands/horario.js
+++ commands/horario.js
@@ -4,6 +4,12 @@ module.exports = {
 	name: 'horario',
 	description: 'guarda y genera el horario de diversos usuarios',
 	execute(message, args) {
-		
+		if (!args.lenght){
+			//help command
+			message.channel.send("content goes here")
+		}
+		else{
+
+		}
 	}
 }
