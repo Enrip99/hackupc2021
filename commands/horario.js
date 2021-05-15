53275c28730cf858bc04d030f6667b5db40d8496
enrip99 <enric.1999@hotmail.com>
2021-05-15 21:27:44 +0200

    small canges

--- commands/horario.js
+++ commands/horario.js
@@ -1,4 +1,5 @@
 const config = require('../data/config.json');
+const config = require('../data/calendar.json');
 
 module.exports = {
 	name: 'horario',
@@ -18,30 +19,43 @@ module.exports = {
 				else{
 					var i
 					var hora
+					var dia
 					for (i = 1; i<args.length; i+=2){
-						if (args[i].length == 1 && args[i].match(/[lmxjvsd]/)){
-							message.channel.send(args[i])
-
+						dia = args[i]
+						if (dia.length == 1 && dia.match(/[lmxjvsd]/)){
 							hora = parseInt(args[i+1],10)
 							if (!isNaN(hora) && hora < 22 && hora > 7){
-								console.log("a")
-							}
+								//main code here, only if valid hour and day
+								hora -=8
+								switch(dia){
+									case('l') dia = 0
+									break
+									case('m') dia = 1
+									break
+									case('x') dia = 2
+									break
+									case('j') dia = 3
+									break
+									case('v') dia = 4
+									break
+									case('s') dia = 5
+									break
+									case('d') dia = 6
+									break
+								}
 
+
+
+							}
 							else{
 								message.channel.send(args[i+1] +" no es una hora válida")
 							}
-
 						}
-
 						else{
 							message.channel.send(args[i] + " no es un día válido. Solo acepto **l**unes, **m**artes, (**x**)miercoles, **j**ueves, **v**iernes, **s**ábado y **d**omingo.")
 						}
 					}
 				}
 			}
-
-
-
 		}
 	}
-}
