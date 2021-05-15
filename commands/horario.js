4255cdf94c500c3d796af7618e16e62c5ad2adcb
enrip99 <enric.1999@hotmail.com>
2021-05-15 16:42:48 +0200

    slow progress

--- commands/horario.js
+++ commands/horario.js
@@ -4,11 +4,26 @@ module.exports = {
 	name: 'horario',
 	description: 'guarda y genera el horario de diversos usuarios',
 	execute(message, args) {
-		if (!args.lenght){
-			//help command
+		if (!args.length){
+			//help command, TO DO
 			message.channel.send("content goes here")
 		}
+
 		else{
+			if (args[0] == "add"){
+				if (args.length == 1 || args.length%2 == 0) {
+					message.channel.send("Comando inválido - Recuerda enviar los días y horas a pares")
+				}
+
+				else{
+					var i
+					for (i = 1; i<args.length; i+=2){
+						message.channel.send(args[i])
+					}
+				}
+			}
+
+
 
 		}
 	}
