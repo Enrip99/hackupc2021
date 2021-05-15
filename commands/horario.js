fd96ebac37a5ea7f9b8edd96033a38b15aa383c2
enrip99 <enric.1999@hotmail.com>
2021-05-16 00:33:07 +0200

    remove function working

--- commands/horario.js
+++ commands/horario.js
@@ -56,14 +56,14 @@ module.exports = {
 								}
 								if (!found){
 									calendar.hor[dia][hora].push(message.author.id)
-									}
+								}
 							}
 							else{
 								message.channel.send(args[i+1] +" no es una hora válida")
 							}
 						}
 						else{
-							message.channel.send(args[i] + " no es un día válido. Solo acepto **l**unes, **m**artes, (**x**)miercoles, **j**ueves, **v**iernes, **s**ábado y **d**omingo.")
+							message.channel.send("`" + args[i] + "` no es un día válido. Solo acepto **l**unes, **m**artes, (**x**)miercoles, **j**ueves, **v**iernes, **s**ábado y **d**omingo.")
 						}
 					}
 					fs.writeFile("./data/calendar.json", JSON.stringify(calendar), function(err) {if (err) console.log(err)})
@@ -102,15 +102,74 @@ module.exports = {
 
 						for (hora = 0; hora < dia.length; ++hora){
 							if (dia[hora].length > 0){
-								msgToSend = msgToSend.concat("A las ",hora,": ", dia[hora].length, " personas\n")
+								msgToSend = msgToSend.concat("A las ",hora+8,": ", dia[hora].length, " personas\n")
 							}
 							else if (dia[hora].length == 1){
-								msgToSend = msgToSend.concat("A las ",hora,": ", dia[hora].length, " persona\n")
+								msgToSend = msgToSend.concat("A las ",hora+8,": ", dia[hora].length, " persona\n")
 							}
 						}
 						if (msgToSend != "")message.channel.send(msgToSend)
 						else message.channel.send("Este día no hay nadie")
 					}
+					else {
+						message.channel.send("`" + dia + "` no es un día válido. Solo acepto **l**unes, **m**artes, (**x**)miercoles, **j**ueves, **v**iernes, **s**ábado y **d**omingo.")
+					}
+				}
+				else{
+					message.channel.send("Argumentos incorrectos. Por favor especifíca un solo día")
+				}
+			}
+			else if (args[0] == "remove"){
+				if (args.length == 1 || args.length%2 == 0) {
+					message.channel.send("Comando inválido - Recuerda enviar los días y horas a pares")
+				}
+
+				else{
+					var i
+					var hora
+					var dia
+					for (i = 1; i<args.length; i+=2){
+						dia = args[i]
+						if (dia.length == 1 && dia.match(/[lmxjvsd]/)){
+							hora = parseInt(args[i+1],10)
+							if (!isNaN(hora) && hora < 22 && hora > 7){
+								hora -=8
+								switch(dia) {
+									case 'l':
+									dia = 0
+									break
+									case 'm':
+									dia = 1
+									break
+									case 'x':
+									dia = 2
+									break
+									case 'j':
+									dia = 3
+									break
+									case 'v':
+									dia = 4
+									break
+									case 's':
+									dia = 5
+									break
+									case 'd':
+									dia = 6
+									break
+								}
+								for(x in calendar.hor[dia][hora]){
+									if (calendar.hor[dia][hora][x] == message.author.id) calendar.hor[dia][hora].splice(x,1)
+								}
+							}
+							else{
+								message.channel.send(args[i+1] +" no es una hora válida")
+							}
+						}
+						else{
+							message.channel.send("`" + args[i] + "` no es un día válido. Solo acepto **l**unes, **m**artes, (**x**)miercoles, **j**ueves, **v**iernes, **s**ábado y **d**omingo.")
+						}
+					}
+					fs.writeFile("./data/calendar.json", JSON.stringify(calendar), function(err) {if (err) console.log(err)})
 				}
 			}
 		}
