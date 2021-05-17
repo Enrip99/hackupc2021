0b08080a5c8e6e1c84e3db3a60fb959ceeee1446
enrip99 <enric.1999@hotmail.com>
2021-05-17 20:49:49 +0200

    cleaned comments

--- commands/horario.js
+++ commands/horario.js
@@ -1,8 +1,6 @@
 const config = require('../data/config.json');
 var calendar = require('../data/calendar.json');
 const fs = require('fs');
-//const Discord = require('discord.js');
-//const client = new Discord.Client();
 
 
 module.exports = {
